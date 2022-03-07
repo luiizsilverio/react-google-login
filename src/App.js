bdecce7443dc82dc531bf5c233dd45f784970729
luiizsilverio <luiiz.silverio@gmail.com>
2022-03-07 20:21:12 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import GoogleLogin from "react-google-login";
+
+function App() {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [picture, setPicture] = useState()
+  const [isLogged, setIsLogged] = useState(false)
+
+  const responseGoogle = (response) => {
+    const { profileObj: { name, email, imageUrl } } = response
+
+    setName(name)
+    setEmail(email)
+    setPicture(imageUrl)
+    setIsLogged(true)
+  }
+
+  const rejectGoogle = (response) => {
+    console.log(response)
+    alert(response.error)
+  }
+
+	return (
+		<div className="container">
+			<GoogleLogin
+				clientId={process.env.REACT_APP_CLIENT_ID}
+				buttonText="Continuar com o Google"
+        onSuccess={responseGoogle}
+        onFailure={rejectGoogle}
+			/>
+      {
+        isLogged &&
+          <div>
+            <h1>Dados do usuário</h1>
+            <img src={picture} alt={name} className="profile" />
+            <p>Nome: {name}</p>
+            <p>E-mail: {email}</p>
+          </div>
+      }
+		</div>
+	);
+}
+
+export default App;
